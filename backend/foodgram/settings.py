496f8326703d29db5d6542a2d14094c68dc401bb
Ekaterina Gvozdeva <0katerishna@gmail.com>
2025-07-27 15:40:37 +0700

    settings changed

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -124,16 +124,9 @@ REST_FRAMEWORK = {
 }
 
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:80',
-    'http://host.docker.internal:80',
-]
+CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS', '').split(',')
 
-
-CSRF_TRUSTED_ORIGINS = [
-    'http://localhost',
-    'http://127.0.0.1'
-]
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split(',')
 
 CORS_ALLOW_CREDENTIALS = True
 
